17c33c4eb675a56d8335869bf838ad4a8f5b35f5
Lucas <Lucas@Lucas-MacBook-Pro.local>
2020-02-10 23:02:16 -0500

    added configuration YAML file

---
+++ modeling/loadConfig.m
@@ -0,0 +1,11 @@
+%
+clear, clc
+
+config_file = 'robot_config.yaml';
+config = ReadYaml(config_file);
+
+
+% config_file = 'robot_config.json';
+% text = fileread(config_file);
+% config = jsondecode(text)
+
