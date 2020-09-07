be25295e01229f0a3f8a081902eb1bdc8c736eea
Lucas <lucas.tiziani@gatech.edu>
2020-09-07 12:15:54 -0400

    changed name from jointTorque; muscle torque calculated from joint state update method of MuscleJoint; added angle range to damping torque

--- modeling/@JumpingRobot/updateTorques.m
+++ modeling/@JumpingRobot/updateTorques.m
@@ -1,4 +1,4 @@
-function [tau, joint_state] = jointTorque(obj,t,x)
+function tau = updateTorques(obj,t,x)
 % calculate joint torques applied by muscle-tendon units
 tau = zeros(7,length(t));
 
@@ -11,25 +11,29 @@ for idx_j = 1:4 % loop over index corresponding to actuated joint
     ang0 = obj.state.x0(idx_q); % initial joint angle
     ang_cur = x(:,idx_q); % current joint angle
     angvel_cur = x(:,idx_xd); % current joint angular velocity
-    joint = obj.config.model.joint_order{idx_j}; % joint name
+    joint_fn = obj.config.model.joint_order{idx_j}; % joint name
+    
+    %TODO: update robot pressures here?
     
     % muscle torque
-    joint_state.(joint) = obj.joints.(joint).calcJointState(ang0, ang_cur,...
-        t-obj.config.control.t_musc_activate(idx_j), obj.config.control.p_max);    
-    joint_state.(joint).torque = sign_tau(idx_j)*joint_state.(joint).torque; 
-    tau_muscle = joint_state.(joint).torque; 
-
+    t_valve = t - obj.config.control.t_musc_activate(idx_j); % time relative to valve open
+    obj.joints.(joint_fn).updateJointState(ang0, ang_cur, t_valve, obj);     
+    tau_muscle = sign_tau(idx_j)*obj.joints.(joint_fn).state.torque;
+    
     % spring torque
-    joint_angle_sign = obj.joints.(joint).get_joint_angle_sign();
+    joint_angle_sign = obj.joints.(joint_fn).get_joint_angle_sign();
     truth_stiff = joint_angle_sign*ang_cur >= ...
         joint_angle_sign*deg2rad(obj.config.model.joint_stiff_lims(1,idx_j));
     tau_stiff = -truth_stiff.*obj.config.model.joint_stiff(idx_j)*...
         (ang_cur-deg2rad(obj.config.model.joint_stiff_lims(1,idx_j)));
     
-    % damping torque (viscous friction)    
-    tau_damp = -obj.config.model.joint_damp(idx_j)*angvel_cur;
+    % damping torque (viscous friction)
+    truth_damp = joint_angle_sign*ang_cur >= ...
+        joint_angle_sign*deg2rad(obj.config.model.joint_damp_lims(1,idx_j));
+    tau_damp = -truth_damp.*obj.config.model.joint_damp(idx_j)*angvel_cur;
+%     tau_damp = -obj.config.model.joint_damp(idx_j)*angvel_cur;
         
     % total torque
     tau(idx_q,:) = tau_muscle + tau_stiff + tau_damp;
-        
+            
 end
