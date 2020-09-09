a862e77f9cead5462cad242d0d4537051633ff35
Lucas <lucas.tiziani@gatech.edu>
2020-09-09 12:13:10 -0400

    joint state name changes; included Rs & T in gas_props

--- modeling/@JumpingRobot/updateGasSource.m
+++ modeling/@JumpingRobot/updateGasSource.m
@@ -5,15 +5,13 @@ function updateGasSource(obj)
     for idx_j = 1:4 % loop over index corresponding to actuated joint
         joint_fn = obj.config.model.joint_order{idx_j}; % joint name
 
-        mdot_source = mdot_source + obj.joints.(joint_fn).state.mdot_gas; 
+        mdot_source = mdot_source + obj.joints.(joint_fn).state.mdot_musc; 
         % update total mass flow out of tank
     end
     
     % update source tank
-    Rs = 287.058; % (J/(kg K))
-    T = 298; % (K)
     obj.state.m_source = obj.state.m_source - mdot_source*obj.sim_param.dt; % (kg) source
     obj.state.p_source = (obj.state.m_source/ ...
-        (obj.config.pneumatic.v_source*1e-3))*Rs*T/1000; % (Pa to kPa)    
-    
+        (obj.config.pneumatic.v_source*1e-3))*...
+        obj.gas_props.Rs*obj.gas_props.T/1000; % (Pa to kPa)    
 end
